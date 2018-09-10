1c2326fc12a5d1e541086190e485f4aef7308194
Leonardo Takuno <ltakuno@10-43-48-57.0200.snsp.br.iosda.org>
2018-09-10 17:46:49 -0300

    Agenda

---
+++ python/agenda/versao2/Agenda.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from Contato import Contato
+contatos = []
+
+def main():
+    sair = False
+    while not sair:
+        opcao = menu_principal()
+
+        if opcao == 1:
+            inserir_contato()
+        elif opcao == 2:
+            buscar_contato()
+        elif opcao == 3:
+            sair = True
+        else: 
+            print('Erro: opção inválida!')
+    print('Fim do programa')
+
+def menu_principal():
+    leia_inteiro = False   
+    opcao = 0 
+    while (not leia_inteiro):
+        print('\nAGENDA TELEFÔNICA')
+        print('(1) - Inserir')
+        print('(2) - Buscar')
+        print('(3) - sair')
+        try:
+            opcao = int(input('Escolha a opção:'))
+            leia_inteiro = True
+        except ValueError:
+            print('Erro: opção inválida!')
+            leia_inteiro = False
+       
+    return opcao
+
+def inserir_contato():
+    print('\nINSERÇÃO DE NOVO CONTATO')
+    nome = ler_nome()
+    telefone = ler_telefone()
+    c1 = Contato(nome,telefone)
+    if contatos.__contains__(c1):
+        print('Este contato já está cadastrado!')
+    else:
+        contatos.append(c1)
+        print('Contato inserido!')
+
+def ler_nome():
+    nome = ''
+    valido = False
+    while (not valido):
+        nome = input('Nome: ')
+        if (len(nome) == 0 or len(nome) > 200):
+            print('ERRO: nome de tamanho inválido!')
+        else:
+            valido = True
+    return nome
+
+def ler_telefone():
+    telefone = ''
+    valido = False
+    while (not valido):
+        telefone = input('Telefone: ')
+        if (len(telefone) == 0 or len(telefone) > 25):
+            print('ERRO: telefone de tamanho inválido!')
+        else:
+            valido = True
+    return telefone
+
+def buscar_contato():
+    print('\nBUSCA DE CONTATO')
+    nome = ler_nome()
+    resultado = []
+    for c in contatos:
+        if nome == c.nome:
+            resultado.append(c)
+
+    if len(resultado) == 0:
+        print('Não há contato com este nome!')
+    else:
+        print('Resultado da Busca')
+        for c in resultado:
+            print(c)
+
+
+main()
