3642fd9cda6a8694597e750f7dad325ac99f7b04
Leonardo Takuno <leonardo.takuno@gmail.com>
2018-09-20 00:43:37 -0300

    agenda

--- python/patterns/Controller.py
+++ python/patterns/Controller.py
@@ -2,15 +2,19 @@
 from Model import Pessoa
 from View import ViewPessoa
 
+class Controller:
+    def __init__(self):
+        self.viewPessoa = ViewPessoa()
 
-def apresentar_conteudo():
-    pessoas = Pessoa.obter_pessoas()
-    ViewPessoa.mostrar_todas(pessoas)
+    def apresentar_conteudo(self):
+        pessoas = Pessoa.obter_pessoas()
+        self.viewPessoa.mostrar_todas(pessoas)
 
-def executar():
-    ViewPessoa.cabecalho()
-    apresentar_conteudo()
-    ViewPessoa.rodape()
+    
+    def executar(self):
+        self.viewPessoa.cabecalho()
+        self.apresentar_conteudo()
+        self.viewPessoa.rodape()
 
 if __name__ == '__main__':
-    executar()
+    Controller().executar()
--- python/patterns/View.py
+++ python/patterns/View.py
@@ -2,16 +2,13 @@
 from Model import Pessoa
 
 class ViewPessoa:
-    @classmethod
     def mostrar_todas(self, pessoas):
         print('Numero de pessoas: %d' % (len(pessoas)))
         for pessoa in pessoas:
             print (pessoa.nome_completo())
 
-    @classmethod
     def cabecalho(self):
         print('Exemplos simples do MVC')
 
-    @classmethod
     def rodape(self):
         print('Fim da Visualização!')
