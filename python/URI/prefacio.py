faa3ae5e26bc39396d266ad82e8a847db1ff4399
Leonardo Takuno <leonardo.takuno@gmail.com>
2018-12-06 07:59:51 -0200

    exercicios

--- python/URI/prefacio.py
+++ python/URI/prefacio.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
+
 def divide(a,b):
-	q = a // b
-	r = a - b * q 
-	if (a>0 and b<0 and r<0):
-		r = -1 * abs(a) + abs(b) * q 
-	return (q, r) 
+	q = a//b
+	r = a%b
+	if r < 0:
+		r = r-b
+		q = (a-r)/b
+	return (q, r)
 
 def teste(obtido, esperado):
 	if (obtido == esperado):
@@ -24,6 +26,7 @@ def main():
 	teste(divide(-3,3), (-1,0))
 	teste(divide(-1,-2),(1,1))
 	teste(divide(-1000,1000),(-1,0))
+        teste(divide(-3,-7), (1,4))
 
 if __name__ == '__main__':
 	main()
--- python/URI/uri1837_prefacio.py
+++ python/URI/uri1837_prefacio.py
@@ -1,33 +1,15 @@
 # -*- coding: utf-8 -*-
-def divide(a,b):
-	q = a // b
-	r = a - b * q
-	if ((a > 0 and b < 0) or (a < 0 and b > 0)) and (r < 0):
-		q = -1 * (abs(a) // abs(b))
-		r = abs(a) % abs(b) 
-	elif (a < 0 and b < 0):
-		q = abs(a) // abs(b)
-		r = abs(a) % abs(b) 
-	return (q, r) 
-
-def teste(obtido, esperado):
-	if (obtido == esperado):
-		prefixo = 'Passou!!!'
-	else:
-		prefixo = 'Erro...'
-	print('%s obtido: %s esperado: %s' % (prefixo, repr(obtido), repr(esperado)))
-
-def main():
-	teste(divide(7,3), (2,1))
-	teste(divide(7,-3), (-2,1))
-	teste(divide(-7,3), (-3,2))
-	teste(divide(-7,-3), (3,2))
-	teste(divide(-9,-3), (3,0))
-	teste(divide(-9,3), (-3,0))
-	teste(divide(-16,3), (-6,2))
-	teste(divide(-3,3), (-1,0))
-	teste(divide(-1,-2),(1,1))
-	teste(divide(-1000,1000),(-1,0))
-
-if __name__ == '__main__':
-	main()
+while True:
+    try:
+        enter=input()
+        enter=enter.split()
+        a,b=int(enter[0]),int(enter[1])
+        q = a // b
+        r = a % b
+        if (r < 0):
+            r = r - b
+            q = (a-r)/b
+    
+        print("%d %d" % (q, r))
+    except:
+        break
