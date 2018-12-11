90879adf3e5144bd73fb448a400174be40dbeed2
Leonardo Takuno <leonardo.takuno@gmail.com>
2018-12-11 11:05:19 -0200

    advent of work

---
+++ C/adventofcode2018/day06/part02/d06.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int distancia_manhatan(int x, int y, int x1, int y1){
+   return abs(x - x1) + abs(y - y1);
+}
+
+struct {
+   int x, y, id, vizinhos;
+} pontos[100], nenhum = {-1,-1,-1,-1};
+
+
+int main(){
+   int x, y;
+   int n = 0;
+   int xfim=0, yfim=0;
+  
+   while (scanf("%d, %d", &x, &y) != EOF){
+	pontos[n].x = x;
+        pontos[n].y = y;
+        pontos[n].id = n;
+        pontos[n].vizinhos = 0;
+
+        if (x > xfim) xfim = x; 
+        if (y > yfim) yfim = y; 
+        n++;
+   }
+
+   int menor_distancia = INT_MAX;
+   int id_menor = -1;
+   int total;
+   int area = 0;
+
+   for (int x1 = 0; x1 <= xfim; x1++) {
+      for (int y1 = 0; y1 <= yfim; y1++) {
+         total = 0;
+         id_menor = -1;
+         menor_distancia = INT_MAX;
+         for (int i = 0; i < n; i++) {
+            int d = distancia_manhatan(pontos[i].x, pontos[i].y, x1, y1); 
+            if (d == menor_distancia){
+               id_menor = -1;
+            } else if (d < menor_distancia) {
+               //printf("menor distancia: %d, id : %d\n", menor_distancia, i);
+               menor_distancia  = d;
+               id_menor = i;
+            }
+            total += d;
+         }
+         if (id_menor != nenhum.id  && pontos[id_menor].vizinhos >= 0) { 
+            pontos[id_menor].vizinhos ++;
+         }
+
+         if (id_menor != nenhum.id && (x1 == 0 || y1 == 0 || x1 == xfim || y1 == yfim)) {
+            pontos[id_menor].vizinhos = -1;
+         }
+
+         if (total < 32)
+             area++;
+      }
+   }
+
+   int maior = INT_MIN;
+   for (int i = 0; i < n; i++){
+      if (pontos[i].vizinhos > maior)
+         maior = pontos[i].vizinhos;
+   }
+
+   printf("Answer 01: %d\n", maior);
+   printf("Answer 02: %d\n", area);
+
+   return 0;
+}
---
+++ C/adventofcode2018/day07/part01/d07.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 200
+
+int n = 0, adj[MAX][MAX] = {0};
+int inicio = -1, fim = -1, fila[MAX];
+int nos[MAX] = {0};
+void insere_fila(int no){
+   if (fim == MAX - 1)
+      printf("Fila cheia \n");
+   else
+   {
+      if (inicio == -1) /* se a fila está inicialmente vazia */
+         inicio = 0;
+
+         fim = fim + 1;
+         fila[fim] = no;
+        
+         int i = fim;
+         while (i > inicio && fila[i-1] > no){
+            fila[i] = fila[i-1];
+            i--;
+         }
+         fila[i] = no;
+         printf("Fila: ");
+         for (i = inicio; i <= fim; i++)
+             printf(" %d ", fila[i]);
+
+
+
+         printf("\n");
+      
+   }
+}
+
+int remove_fila()
+{
+   int item;
+   if (inicio == -1 || inicio > fim) {
+      printf("Fila Underflow \n");
+      exit(1); 
+   }
+   else
+   {
+      item = fila[inicio];
+      inicio = inicio + 1;
+   }
+   return item;
+}
+
+int calcula_grau_entrada(int no){
+   int i, grau = 0;
+
+   for (i = 0; i <= n; i++)
+      if (adj[i][no] == 1)
+         grau++;
+   printf("Grau de entrada do no %c : %d\n", no + 'A', grau);
+   return grau;
+}
+
+void leia_grafo(){
+   char origem, destino;
+   while(scanf("Step %c must be finished before step %c can begin.\n", &origem, &destino) != EOF){
+      printf("Origem %c, Destino %c\n ", origem, destino);
+
+      adj[origem - 'A'][destino - 'A'] = 1;
+      nos[origem - 'A'] = 1;
+      nos[destino - 'A'] = 1; 
+   }
+}
+
+
+int main()
+{
+   leia_grafo();
+   int ordenacao_topologica[MAX] = {0}, grau_entrada[MAX] = {0};
+   int no, k = 0;
+   n = 'Z' - 'A';
+   for (int i = 0; i <= n; i++) {
+      if (nos[i]){
+         grau_entrada[i] = calcula_grau_entrada(i);
+         if (grau_entrada[i] == 0) 
+            insere_fila(i);
+      }
+   }
+
+   while (inicio <= fim) {
+      no = remove_fila();
+      ordenacao_topologica[k++] = no;
+      for (int i = 0; i <= n; i++) /* remover arestas a partir de no */
+      {
+         if (adj[no][i] == 1)
+         {
+            adj[no][i] = 0;
+            grau_entrada[i] = grau_entrada[i] - 1;
+            if (grau_entrada[i] == 0)
+               insere_fila(i);
+         }
+      }
+   } 
+
+   printf("Nos depois da ordenacao topologica: \n");
+   for (int i = 0; i < k; i++)
+       printf("%c", ordenacao_topologica[i] + 'A'); 
+   printf("\n");
+   return 0;
+}
